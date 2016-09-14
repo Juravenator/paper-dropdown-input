3f55fec382f8c875cc59e5b967deb287231bfa1a
Glenn Dirkx <glenn.dirkx@agilians.com>
2016-09-14 14:24:00 +0200

    added support for the noFreedom function

--- src/babel/paper-dropdown-input.js
+++ src/babel/paper-dropdown-input.js
@@ -15,14 +15,49 @@ Polymer({
     },
     _filtereditems: {
       type: Array,
-      computed: '_filterItems(items, value)',
+      computed: '_filterItems(items, immediateValue)',
       observer: '_updateTemplate'
     },
+
+    _inputvalue: {
+      type: String,
+      value: "",
+      observer: "_inputvalueChanged"
+    },
+    /**
+     * The immediate value of the input.
+     * This value is updated while the user is typing.
+     */
+    immediateValue: {
+      type: String,
+      value: "",
+      readOnly: true,
+      notify: true
+    },
+    /**
+     * The string that represents the current value.
+     */
+    value: {
+      type: String,
+      value: "",
+      // notify is already fired (twice) by Polymer.IronFormElementBehavior and Polymer.PaperInputBehavior
+      // notify: true,
+      observer: "_valueChanged"
+    },
+
     _templateInstance: Object,
 
     opened: {
       type: Boolean,
       observer: "_openedChanged"
+    },
+
+    /**
+     * if true, this input doesn't allow any value that does not exist in the 'items' array
+     */
+    noFreedom: {
+      type: Boolean,
+      value: false
     },
 
     animationConfig: {
@@ -60,14 +95,14 @@ Polymer({
     'neon-animation-finish': '_onNeonAnimationFinish'
   },
 
-  _filterItems: function(items, value) {
+  _filterItems: function(items, immediateValue) {
     var result = [];
     for (var i = 0; i < items.length; i++) {
       var include = false;
       if (items[i].value) {
-        include = items[i].value.toLowerCase().indexOf(value) > -1;
+        include = items[i].value.toLowerCase().indexOf(immediateValue) > -1;
       } else if (typeof items[i] === 'string' || items[i] instanceof String) {
-        include = items[i].toLowerCase().indexOf(value) > -1;
+        include = items[i].toLowerCase().indexOf(immediateValue) > -1;
       } else {
         console.error("paper-dropdown-input: item in `items`:", items[i], " is not a string or does not contain `value` property")
       }
@@ -81,7 +116,7 @@ Polymer({
   _onClick: function(event) {
     var selectedItem = Polymer.dom(event).localTarget;
     if (selectedItem) {
-      this.value = selectedItem.value || selectedItem.label || selectedItem.textContent.trim();
+      this._inputvalue = selectedItem.value || selectedItem.label || selectedItem.textContent.trim();
     }
   },
 
@@ -113,11 +148,11 @@ Polymer({
   },
 
   _clearInput: function() {
-    this.value = "";
+    this._inputvalue= "";
   },
 
-  _getIcon: function(value) {
-    if (value && value != "") {
+  _getIcon: function(immediateValue) {
+    if (immediateValue && immediateValue != "") {
       return "close"
     } else {
       return "arrow-drop-down"
@@ -133,7 +168,55 @@ Polymer({
     // console.log("animation finished");
     if (!this.opened) {
       this.$.menu.style.display = "none";
+      this._handleNewValue();
     }
   },
+
+  _handleNewValue: function() {
+    // if there is only one match, we auto-fill it
+    if (this._filtereditems.length == 1) {
+      this._inputvalue = this._filtereditems[0];
+      this._setImmediateValue(this._inputvalue);
+    }
+
+
+
+    // just update the value if any input is allowed
+    if (!this.noFreedom) {
+      this.value = this.immediateValue;
+
+    // if noFreedom is set, check for valid input and revert invalid input
+    } else {
+      var match = false;
+
+      for (var i = 0; i < this.items.length; i++) {
+        var item = this.items[i];
+        if (item.value && item.value == this.immediateValue) {
+          match = true;
+        } else if (typeof item === 'string' || item instanceof String) {
+          if (item == this.immediateValue) {
+            match = true;
+          }
+        }
+      }
+
+      // reset to original value if no match is found
+      if (!match) {
+        this._inputvalue = this.value;
+      } else {
+        this.value = this.immediateValue;
+      }
+    }
+  },
+
+  _valueChanged: function(newval, oldval) {
+    this._inputvalue = newval;
+    this._setImmediateValue(newval);
+    this._handleNewValue();
+  },
+
+  _inputvalueChanged: function(value) {
+    this._setImmediateValue(value);
+  }
 
 });
